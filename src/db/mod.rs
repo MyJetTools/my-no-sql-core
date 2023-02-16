09dc2bb238e64926ada9b042e2fd5df22ba46e91
amigin <amigin@gmail.com>
2023-02-16 19:24:31 +0200

    DbInstance goes to the server-core

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -4,12 +4,10 @@ pub use db_partition::{DbPartition, UpdatePartitionReadMoment};
 
 pub use db_row::DbRow;
 
-mod db_instance;
 mod db_partition;
 pub mod db_snapshots;
 
 mod db_row;
 mod db_table;
 mod update_expiration_time_model;
-pub use db_instance::*;
 pub use update_expiration_time_model::UpdateExpirationTimeModel;
