25e57194f0da3801c33d1c4ebacc119e64d84f14
amigin <amigin@gmail.com>
2023-02-22 15:05:48 +0200

    moved sync to main to the tcp_shared layer

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 pub mod db;
 pub mod db_json_entity;
-pub mod sync_to_main;
 #[cfg(feature = "master_node")]
 mod utils;
 pub mod validations;
--- src/sync_to_main/sync_to_main_node_event.rs
+++
@@ -1,8 +0,0 @@
-use std::sync::Arc;
-
-pub enum SyncToMainNodeEvent<DataReaderTcpConnection: Send + Sync + 'static> {
-    Connected(Arc<DataReaderTcpConnection>),
-    Disconnected(Arc<DataReaderTcpConnection>),
-    PingToDeliver,
-    Delivered(i64),
-}
